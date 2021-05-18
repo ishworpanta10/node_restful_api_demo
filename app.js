cc2d9dcf7412e87da8fa05e9af660cb143264618
ishworpanta10 <ishworpanta10@gmail.com>
2021-05-18 13:30:51 +0545

    handled CORS Errors

--- app.js
+++ app.js
@@ -18,6 +18,21 @@ app.use(
 // app.use(bodyParser.urlencoded({ extended = false }))
 // app.use(bodyParser.json());
 
+// CORS  enable to request for all client
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 // Routes which should handle request
 app.use('/products', productRoute);
 app.use('/orders', orderRoute);
